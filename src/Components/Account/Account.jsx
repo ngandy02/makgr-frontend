c629d1389b43484e9d23455f0aad954a9934724c
Andy Ng <ngandy0202@gmail.com>
2025-03-18 21:28:15 -0400

    Added Manage Acc Button

--- src/Components/Account/Account.jsx
+++ src/Components/Account/Account.jsx
@@ -12,9 +12,9 @@ function Account() {
      <h1 className="text-md font-bold">Email</h1>
      <h1 className="text-md font-bold">Password</h1>
      <h1 className="text-md font-bold">Role</h1>
+     <h1 className="text-md font-bold">Date Created</h1>
 
-
-
+     <button className="mt-2">Manage Account</button>
     </div>
   )
 }
