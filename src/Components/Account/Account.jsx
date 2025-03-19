9b63d459be648d13ff7b98acf9200f74a5b645be
Andy Ng <ngandy0202@gmail.com>
2025-03-18 21:22:41 -0400

    Added Account Headings

--- src/Components/Account/Account.jsx
+++ src/Components/Account/Account.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+// import propTypes from "prop-types";
+// import axios from "axios";
+// import { Link } from "react-router-dom";
+
+// import { BACKEND_URL } from "../../constants";
 
 function Account() {
-  return <h1>My Account</h1>;
+  return (
+    <div className="wrapper">
+     <h1 className="text-lg font-bold">Name of Person</h1>
+     <h1 className="text-md font-bold">Email</h1>
+     <h1 className="text-md font-bold">Password</h1>
+     <h1 className="text-md font-bold">Role</h1>
+
+
+
+    </div>
+  )
 }
 
 export default Account;
